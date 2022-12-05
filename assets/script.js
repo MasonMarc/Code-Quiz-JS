066b24431892bcd8bf5c581219f261be176e4290
MasonMarc <masonmarcantel@gmail.com>
2022-12-05 18:59:56 -0500

    added functionality to start page and replaced question placeholders

--- assets/script.js
+++ assets/script.js
@@ -2,50 +2,51 @@ var questionEl = document.querySelector('.question');
 var buttonEl = document.querySelector('.possible');
 var timeEl = document.querySelector(".time");
 var resultEl = document.querySelector(".result");
+var mainEl = document.querySelector('.wrapper');
 var timeleft = 100;
 var cursor = 0;
 var score = 0;
 
-
+// object of all possible
 var questions = [
     {
-        text: "Question 1",
+        text: "What Does 'HTML' Stand for?",
         correctAnswer: "a",
         possible: [
-            "a. 10",
-            "b. 20",
-            "c. 30",
-            "d. 40",
+            "a. HyperText Markup Language",
+            "b. Homing Text Mobile Language",
+            "c. Hartman Tonic Meyers Lang",
+            "d. Hacking Text Markup Language",
         ]
     },
     {
-        text: "Question 2",
+        text: "What is an element in JavaScript that can only be either a 'True' or a 'False' value?",
         correctAnswer: "b",
         possible: [
-            "a. 100",
-            "b. 200",
-            "c. 300",
-            "d. 400",
+            "a. Variable",
+            "b. Boolean",
+            "c. String",
+            "d. Event",
         ]
     },
     {
-        text: "Question 3",
+        text: "What does 'CSS' stand for?",
         correctAnswer: "c",
         possible: [
-            "a. 1",
-            "b. 2",
-            "c. 3",
-            "d. 4",
+            "a. Computer Style Source",
+            "b. Cucumber Sriracha Salad",
+            "c. Cascading Style Sheets",
+            "d. Coding Sheet Syntax",
         ]
     },
     {
-        text: "Question 4",
+        text: "In Javascript, what is the method to remove the last object of an array",
         correctAnswer: "d",
         possible: [
-            "a. a",
-            "b. b",
-            "c. c",
-            "d. d",
+            "a. array - 1",
+            "b. array.reduce()",
+            "c. array.remove()",
+            "d. array.pop()",
         ]
     }
 ]
@@ -53,24 +54,25 @@ var questions = [
 var correctAnswers = ["a", "b", "c", "d"];
 
 // Makes boxes display actual text of question object
-var displayQuestion = function () {
+function displayQuestion() {
+    startEl.style.display = "none";
+    mainEl.style.display = "flex";
+    Timer();
     questionEl.querySelector('h2').textContent = questions[cursor].text;
     buttonEl.querySelector('#a').textContent = questions[cursor].possible[0];
     buttonEl.querySelector('#b').textContent = questions[cursor].possible[1];
     buttonEl.querySelector('#c').textContent = questions[cursor].possible[2];
     buttonEl.querySelector('#d').textContent = questions[cursor].possible[3];
-    if (cursor => questions.length){
+    if (cursor >= questions.length){
         return;
     }
 };
 // --------------------------------------
 
 // advance on button click function
-var advance = function (event) {
+function advance(event) {
     var element = event.target;
-    
-    console.log(element);
-    
+ 
     if (element.matches('button')) {
         var answer = element.dataset.choice === correctAnswers[cursor];
         if(answer){
@@ -78,11 +80,8 @@ var advance = function (event) {
         } else{
             timeleft = timeleft-20;
         }
-        if (cursor < questions.length-1) {
-            console.log(answer);
+        if (cursor < questions.length-1) {      
             cursor++;
-            console.log(cursor);
-            console.log(score);
             resultEl.textContent = "That was " + answer;
             displayQuestion();
         }
@@ -91,9 +90,10 @@ var advance = function (event) {
             questionEl.textContent = "Good Job! " + "You have scored a " + score + "/" + questions.length + "!";
             cursor++;
             // save initial and score in local
-            var initials = prompt("Enter your Initials to be saved ini the high scores:");
+            var initials = prompt("Enter your Initials to be saved in the high score:");
             localStorage.setItem('Initials', initials);
             localStorage.setItem('Score', score);
+            // now make functionality for high scores to stack on completion to allow for multiple scores
         }
     }
 }
@@ -117,9 +117,9 @@ function Timer(){
             buttonEl.style.display = "none";
             questionEl.textContent = "Out of Time! " + "You have scored a " + score + "/" + questions.length + "!";
         }
-    }, 100)
+    }, 1000)
 }
-Timer();
+
 // END TIMER ----------------------------------
 
 
@@ -133,11 +133,14 @@ highEl.addEventListener('click', highscore);
 
 // START PAGE  -----------------
 // make html elements outside of wrapper that has button and header such that when you hit the button the new elements are hidden and the wrapper is shown and timer function and display question are run
+var startEl = document.getElementById('intro');
 
+mainEl.style.display = "none";
+function start(){
+    displayQuestion();
+}
+startEl.addEventListener('click', start);
 // START PAGE END ----------------
 
 
-
-document.addEventListener('click', advance);
-
-displayQuestion();
+buttonEl.addEventListener('click', advance);
